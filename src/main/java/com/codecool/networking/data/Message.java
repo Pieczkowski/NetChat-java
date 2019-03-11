b35d083d2f3f4fb16415fb520f1f4b52eeda66bf
Pieczkowski <pieczkowski.t@gmail.com>
2019-03-11 16:15:43 +0100

    Add receiver field to Message

--- src/main/java/com/codecool/networking/data/Message.java
+++ src/main/java/com/codecool/networking/data/Message.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 public class Message implements Serializable {
     private String content;
     private String author;
+    private String receiver;
     private LocalDateTime createdAt;
 
     public Message(String content, String author) {
@@ -18,16 +19,14 @@ public class Message implements Serializable {
         return content;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    @Override
+    public String getReceiver() {
+        return receiver;
     }
 
+    @Override
     public String getAuthor() {
         return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
     }
 
     @Override
