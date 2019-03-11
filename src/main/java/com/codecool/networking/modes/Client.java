7b8c86977484c8747a041f044688be4691ea1fed
Pieczkowski <pieczkowski.t@gmail.com>
2019-03-11 16:23:18 +0100

    Implement Client

--- src/main/java/com/codecool/networking/modes/Client.java
+++ src/main/java/com/codecool/networking/modes/Client.java
@@ -1,4 +1,28 @@
 package com.codecool.networking.modes;
 
+import com.codecool.networking.tasks.client.ClientFlowController;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+    private String hostName;
+    private int port;
+
+    public Client(int port, String hostName){
+        this.port = port;
+        this.hostName = hostName;
+    }
+
+    public void start() {
+        System.out.println("Launching Client");
+
+        try(Socket socket = new Socket(hostName, port)){
+            new ClientFlowController(socket);
+            System.out.println("Client Launched");
+            while (true){}
+        }catch (IOException e){
+            System.out.println("Could not find host");
+            System.exit(0);
+        }
+    }
 }
